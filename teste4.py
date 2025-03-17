2d262317d4994dbaf63bedc4a2ce476d26285996
Elias Gomes <eliasgomes@appstorm.com.br>
2025-03-16 23:17:46 -0300

    Atualizando os projetos

--- teste4.py
+++
@@ -1,9 +0,0 @@
-def jogar():
-    opcoes = ["pedra", "papel", "tesoura"]
-
-    # Perguntar ao jogador a escolha
-    jogador = input("Digite pedra, papel ou tesoura: ").lower()
-    print(f"Você escolheu: {jogador}")  # Apenas para mostrar que capturou corretamente
-
-# Chamar a função para executar o jogo
-jogar()
